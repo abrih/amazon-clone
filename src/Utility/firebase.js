383a0ea98921cbd2b275f5daef489bdc37d9f170
Theo123-art <abrish.fikre@gmail.com>
2025-03-06 19:28:56 -0500

    firebase back end

---
+++ src/Utility/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCzNEdFeeT2RdWZWK1F5Q61IZPDB5PHN5s",
+    authDomain: "clone-22796.firebaseapp.com",
+    projectId: "clone-22796",
+    storageBucket: "clone-22796.firebasestorage.app",
+    messagingSenderId: "897790875500",
+    appId: "1:897790875500:web:060e18028c59f1d265c549"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
